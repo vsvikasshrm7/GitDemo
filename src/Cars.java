ddda2a1230e3e0ab437d2706326e6d3ae02db330
vsvikasshrm7 <vsvikasshrm7@gmail.com>
2024-02-26 21:52:30 +0530

    Added new Car Comparator

--- src/Cars.java
+++ src/Cars.java
@@ -2,6 +2,22 @@ public class Cars {
     private int price;
     private int size;
 
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     public Cars(int price, int size) {
         this.price = price;
         this.size = size;
